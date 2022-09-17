6123d6c5441a45964e8a7774a7b407ff1d87e0e3
Donovan Duperrois <slashhable@gmail.com>
2022-09-17 13:07:24 +0200

    ajout de la nav bar, deconnection,+ logo

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse,faUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faHouse,faUser, faRightFromBracket,faMusic, faUserGroup } from '@fortawesome/free-solid-svg-icons';
 import style from "./nav.module.css"
+import { useDispatch, useSelector } from "react-redux"
+import { logoutUser } from '../../store/actions/auth-action';
 
 const Nav = () => {
+    const dispatch= useDispatch()
+
+    const handleLogout = (e) =>{
+        dispatch(logoutUser())
+
+    }
     return (
         <div className={style.navbar}>
             <div className={style.navimg}>
-                {/* <img src="" alt="" /> */}
-                image
+                <img src="https://zupimages.net/up/22/37/rwxy.png" alt="" className={style.logo}/>
             </div>
             <div className={style.menu}>
-                    <h2><div className={style.icon}><FontAwesomeIcon icon={faHouse} /></div> Home</h2>
-                    <h2><div className={style.icon}><FontAwesomeIcon icon={faUser} /></div> Profil</h2>
-                    
-            </div>
+                    <h2><div className={style.icon}><FontAwesomeIcon icon={faHouse} /></div></h2>
+                    <h2><div className={style.icon}><FontAwesomeIcon icon={faMusic} /></div></h2>
+                    <h2><div className={style.icon}><FontAwesomeIcon icon={faUserGroup} /></div></h2>                    
+            </div >
+            
             <div className={style.logout}>
-                <FontAwesomeIcon icon={faRightFromBracket} />
+                <FontAwesomeIcon icon={faUser} />
+                
+                <FontAwesomeIcon icon={faRightFromBracket} onClick={handleLogout}/>
             </div>
         </div>
     );
